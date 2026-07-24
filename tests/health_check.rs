73d350f9e8da4eb2ff23ba018046ace6a9a506db
Kurlgargyey <nahsirkivar@gmail.com>
2026-07-24 23:50:06 +0200

    test for email validation in integration tests and leave detailed name validation testing to the unit tests

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -143,43 +143,14 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 }
 
 #[tokio::test]
-async fn subscribe_returns_a_400_when_name_is_invalid() {
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
     let app = spawn_app().await;
     let client = reqwest::Client::new();
 
     let test_cases = vec![
-        ("name=&email=fake@mail.com", "empty name"),
-        (
-            "ursulaursulaursulaursulaursulaursulaursulaursulaursulaursula
-
-ursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursula
-
-ursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursula
-
-ursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursula
-
-ursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursula
-ursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursula
-ursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursulaursula
-
-        ",
-            "name too long",
-        ),
-        ("name=(&email=fine@com.com", "name contains open paren"),
-        ("name=)&email=fine@com.com", "name contains closed paren"),
-        ("name=/&email=fine@com.com", "name contains forward slash"),
-        ("name=\"&email=fine@com.com", "name contains double quote"),
-        ("name=<&email=fine@com.com", "name contains lt"),
-        ("name=>&email=fine@com.com", "name contains gt"),
-        ("name=\\&email=fine@com.com", "name contains backslash"),
-        (
-            "name={&email=fine@com.com",
-            "name contains open curly brace",
-        ),
-        (
-            "name=}&email=fine@com.com",
-            "name contains closed curly brace",
-        ),
+        ("name=&email=ursula@leguin.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
     ];
 
     for (invalid_body, error_message) in test_cases {
