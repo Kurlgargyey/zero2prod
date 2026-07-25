f50b62ae3b3d270a7c4cd51ec29e8392ba46b21a
Kurlgargyey <nahsirkivar@gmail.com>
2026-07-25 23:27:13 +0200

    add email client initialization to relevant functions

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
-use zero2prod::email_client::{self, EmailClient};
+use zero2prod::email_client::EmailClient;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
@@ -20,7 +20,10 @@ async fn main() -> Result<(), std::io::Error> {
 
     let listener = TcpListener::bind(address)?;
 
-    let email_client = EmailClient::new(base_url, sender);
-    run(listener, connection_pool)?.await;
-    Ok(())
+    let sender_email = configuration
+        .email_client
+        .sender()
+        .expect("Invalid sender email address.");
+    let email_client = EmailClient::new(configuration.email_client.base_url, sender_email);
+    run(listener, connection_pool, email_client)?.await
 }
