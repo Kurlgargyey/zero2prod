3536254366cf285ed51732eb667d9cf7376719d9
Kurlgargyey <nahsirkivar@gmail.com>
2026-07-30 21:58:43 +0200

    TEST: new subscribers should be added as pending confirmation PATCH: new subscribers are added as pending confirmation

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -89,7 +89,7 @@ pub async fn insert_subscriber(data: &NewSubscriber, db_pool: &PgPool) -> Result
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at, status)
-        VALUES ($1, $2, $3, $4, 'confirmed')
+        VALUES ($1, $2, $3, $4, 'pending_confirmation')
         "#,
         Uuid::new_v4(),
         data.email.as_ref(),
